19e45ed5ac1674a4a298b403f92c98ffde90cd62
David Fokkema <davidfokkema@icloud.com>
2025-02-04 09:43:46 +0100

    Better icons on Windows

--- src/ocean_optics/resources/create_icons.sh
+++ src/ocean_optics/resources/create_icons.sh
@@ -5,7 +5,6 @@
         output_iconset_name=$1.iconset
 
         mkdir $output_iconset_name
-
         sips -z 16 16     $input_filepath --out "${output_iconset_name}/icon_16x16.png"
         sips -z 32 32     $input_filepath --out "${output_iconset_name}/icon_16x16@2x.png"
         sips -z 32 32     $input_filepath --out "${output_iconset_name}/icon_32x32.png"
@@ -17,11 +16,24 @@
         sips -z 512 512   $input_filepath --out "${output_iconset_name}/icon_512x512.png"
 
         iconutil -c icns $output_iconset_name
-
-        sips -z 48 48     $input_filepath --out "${output_iconset_name}/icon_48x48.png"
-        icotool -c -o $1.ico "${output_iconset_name}/icon_16x16.png" "${output_iconset_name}/icon_32x32.png" "${output_iconset_name}/icon_48x48.png" "${output_iconset_name}/icon_256x256.png"
-
         rm -R $output_iconset_name
     }
 
+    Create_Win_Icons() {
+        input_filepath=$1.png
+        output_icon_name=$1.ico
+        build_dir=build_ico
+
+        mkdir $build_dir
+        sips -z 16 16     $input_filepath --out "${build_dir}/icon_16x16.png"
+        sips -z 24 24     $input_filepath --out "${build_dir}/icon_24x24.png"
+        sips -z 32 32     $input_filepath --out "${build_dir}/icon_32x32.png"
+        sips -z 48 48     $input_filepath --out "${build_dir}/icon_48x48.png"
+        sips -z 256 256   $input_filepath --out "${build_dir}/icon_256x256.png"
+
+        icotool -c -o $output_icon_name ${build_dir}/*
+        rm -R $build_dir
+    }
+
     Create_Icons app_icon
+    Create_Win_Icons app_icon_win
